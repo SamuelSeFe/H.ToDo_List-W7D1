89733530a1b3004481d400c6a7e016980f09bffd
Samuel <samuelsf@hotmail.co.uk>
2022-08-01 15:09:10 +0100

    finished extensions

--- src/App.js
+++ src/App.js
@@ -1,23 +1,95 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  
+  const [todos, setTodos] = useState ([
+    {name: "Buy shopping", priority: "high"},
+    {name: "Clean bathroom", priority: "low"},
+    {name: "Prep weekly food", priority: "high"}
+  ]);
+
+  const [newTodos, setNewTodos] = useState("");
+
+  const deleteItem = (event, index) => {
+    event.preventDefault();
+    const copyTodos = [...todos];
+    copyTodos.pop(index);
+    setTodos(copyTodos);
+  }
+
+  const todosNodes = todos.map ( (item, index) => {
+    return (
+      <li key={index} className={item.priority === "high" ? "high" : "low"}>
+        <span>
+          {item.name}
+        </span>
+        {(item.priority === "high") ? <span className="high">High</span> : <span className="low">Low</span>
+        }
+        <form onSubmit={deleteItem}>
+          <button onClick= { () => deleteItem(index) }>Done!</button>
+        </form>
+        </li>
+    )
+  })
+  
+  const handleTodosInput = (event) => {
+    setNewTodos(event.target.value);
+  }
+
+// const radioButtons = document.querySelectorAll('input[name="someName"]');
+// var selectedRadioButton;
+// for (const radioButton of radioButtons) {
+//   if (radioButton.checked) {
+//     selectedRadioButton = radioButton.value;
+//     break;
+//   }
+// }
+
+  // const radioButton = document.querySelector('priority').value;
+
+  const saveNewTodos = (event) => {
+    event.preventDefault()
+    const copyTodos = [...todos]
+    copyTodos.push ({
+      name: newTodos,
+      priority: event.target.priority.value
+    });
+    setTodos(copyTodos);
+    setNewTodos("");
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h1>ToDo's</h1>
+      <hr></hr>
+
+      <form onSubmit={saveNewTodos}>
+        <label htmlFor="new-todos">Add new todo item:</label>
+        <input 
+        id="new-todos"
+        type="text"
+        value={newTodos}
+        onChange = {handleTodosInput} />
+
+        <input type="radio" name="priority" value="high" required/>
+        <label htmlFor="high-todos">High</label>
+
+        <input type="radio" name="priority" value="low" required/>
+        <label htmlFor="high-todos">Low</label>
+
+        <input type="submit" value="Save new todos" />
+      </form>
+
+
+
+      <ul>
+        {todosNodes}
+      </ul>
+
+
+      
     </div>
   );
 }
